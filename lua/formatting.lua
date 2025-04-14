b17b0ff92fdbfa9a29263f8b2d833a515609e195
Nathan <delmarchenathan@gmail.com>
2025-04-14 11:43:56 +0200

    Python configuration

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -3,7 +3,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
     callback = function()
         -- Run clang-format using the local .clang-format file
+        local pos = vim.api.nvim_win_get_cursor(0) -- cursos position
         vim.cmd("silent! %!clang-format")
-
+        vim.api.nvim_win_set_cursor(0, pos)
     end,
 })
